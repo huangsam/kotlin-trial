703f4d0f5b122a1ef2d3ea8d18fae53be90806b3
Samuel Huang <samhuang91@gmail.com>
2024-03-17 23:40:57 -0700

    Apply ktlint formatting

--- src/main/kotlin/com/huangsam/lambda/IntegerFun.kt
+++ src/main/kotlin/com/huangsam/lambda/IntegerFun.kt
@@ -3,7 +3,10 @@ package com.huangsam.lambda
 object IntegerFun {
     fun filteredEvenCount(list: List<Int>): Int = list.filter { it % 2 == 0 }.fold(0) { acc, _ -> acc + 1 }
 
-    fun scaleValues(list: List<Int>, factor: Int): List<Int> = list.map { it * factor }
+    fun scaleValues(
+        list: List<Int>,
+        factor: Int,
+    ): List<Int> = list.map { it * factor }
 
     fun sumOfSquares(list: List<Int>): Int = list.fold(0) { acc, i -> acc + i * i }
 }
