8e5871c83347340d8d74d72170c3990dc212c6d0
Samuel Huang <samhuang91@gmail.com>
2024-03-17 19:57:16 -0700

    Refactor filteredEvenCount to be shorter

--- src/main/kotlin/com/huangsam/lambda/IntegerFun.kt
+++ src/main/kotlin/com/huangsam/lambda/IntegerFun.kt
@@ -1,13 +1,8 @@
 package com.huangsam.lambda
 
 object IntegerFun {
-    fun filteredEvenCount(intList: List<Int>): Int {
-        var evenCount = 0
-        intList.filter { it % 2 == 0 }.forEach { _ ->
-            evenCount += 1
-        }
-        return evenCount
-    }
+    fun filteredEvenCount(intList: List<Int>): Int =
+        intList.filter { it % 2 == 0 }.fold(0) { acc, _ -> acc + 1 }
 
     fun scaleValues(intList: List<Int>, factor: Int): List<Int> =
         intList.map { it * factor }
