3d11f622036b270a83cbb5d05a965b2fa218121a
Samuel Huang <samhuang91@gmail.com>
2024-03-18 17:03:52 -0700

    Extend argument captor test

--- src/test/kotlin/com/huangsam/MockitoTest.kt
+++ src/test/kotlin/com/huangsam/MockitoTest.kt
@@ -9,6 +9,7 @@ import org.mockito.Spy
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.reset
+import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
@@ -20,6 +21,12 @@ class MockitoTest {
     @Spy
     private var spyList: MutableList<Int> = mutableListOf()
 
+    private val one = 1
+    private val two = 2
+    private val three = 3
+    private val four = 4
+    private val twoHundred = 200
+
     @BeforeEach
     fun resetMockitoAssets() {
         reset(mockedList, spyList)
@@ -28,27 +35,37 @@ class MockitoTest {
 
     @Test
     fun checkAddCountWithMockedList() {
-        mockedList.add(1)
-        verify(mockedList).add(1)
+        mockedList.add(one)
+        verify(mockedList).add(one)
         assertEquals(0, mockedList.count())
-        whenever(mockedList.count()).thenReturn(200)
-        assertEquals(200, mockedList.count())
+        whenever(mockedList.count()).thenReturn(twoHundred)
+        assertEquals(twoHundred, mockedList.count())
     }
 
     @Test
     fun checkAddCountWithSpyList() {
-        spyList.add(1)
-        spyList.add(2)
-        verify(spyList).add(1)
-        verify(spyList).add(2)
+        spyList.add(one)
+        spyList.add(two)
+        verify(spyList).add(one)
+        verify(spyList).add(two)
         assertEquals(2, spyList.count())
+        whenever(mockedList.count()).thenReturn(twoHundred)
+        assertEquals(twoHundred, mockedList.count())
     }
 
     @Test
-    fun checkAddWithOneArgumentCaptor() {
+    fun checkAddWithArgumentCaptor() {
         val captor = argumentCaptor<Int>()
-        mockedList.add(1)
-        verify(mockedList).add(captor.capture())
-        assertEquals(1, captor.lastValue)
+        mockedList.add(one)
+        mockedList.add(two)
+        mockedList.add(three)
+        mockedList.add(four)
+        captor.apply {
+            verify(mockedList, times(four)).add(capture())
+            assertEquals(one, firstValue)
+            assertEquals(two, secondValue)
+            assertEquals(three, thirdValue)
+            assertEquals(four, lastValue)
+        }
     }
 }
