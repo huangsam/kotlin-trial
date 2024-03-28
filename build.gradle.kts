0d3f768ec4e2ce12d81658fbd6b3d0c5d69b1729
Samuel Huang <samhuang91@gmail.com>
2024-03-28 00:40:23 -0700

    Add links for the dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,11 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.23"
+
+    // https://github.com/JLLeitschuh/ktlint-gradle
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+
+    // https://github.com/ben-manes/gradle-versions-plugin
     id("com.github.ben-manes.versions") version "0.51.0"
 }
 
@@ -10,15 +14,22 @@ group = "com.huangsam"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    // https://mvnrepository.com/repos/central
     mavenCentral()
 }
 
 dependencies {
+    // https://www.slf4j.org/manual.html
     implementation("org.slf4j:slf4j-api:2.0.12")
     implementation("ch.qos.logback:logback-classic:1.5.3")
+
+    // https://www.baeldung.com/mockito-junit-5-extension
     testImplementation("org.mockito:mockito-core:5.11.0")
     testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
+
+    // https://github.com/mockito/mockito-kotlin
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
 
