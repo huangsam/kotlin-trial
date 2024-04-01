34a5e790cc503d268842f2840c9a44af394fb9aa
Samuel Huang <samhuang91@gmail.com>
2024-04-01 01:29:32 -0700

    Adjust Hello class slightly

--- src/main/kotlin/com/huangsam/hello/Hello.kt
+++ src/main/kotlin/com/huangsam/hello/Hello.kt
@@ -2,24 +2,24 @@ package com.huangsam.hello
 
 import org.slf4j.LoggerFactory
 
+private const val HELLO_HELLO = "Hello hello"
+
 class Hello {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    private val helloString = "Hello world"
-
     fun say() {
-        logger.info(helloString)
+        logger.info(HELLO_HELLO)
     }
 
     fun yell() {
-        logger.warn(helloString.uppercase())
+        logger.warn(HELLO_HELLO.uppercase())
     }
 
     fun whisper() {
-        logger.debug(helloString.lowercase())
+        logger.debug(HELLO_HELLO.lowercase())
     }
 
     fun address(name: String) {
-        logger.info("$helloString $name")
+        logger.info("$HELLO_HELLO $name")
     }
 }
