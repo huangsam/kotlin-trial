df674bac53788db155517a9b51ba7d3cc38e4ffc
Samuel Huang <samhuang91@gmail.com>
2024-04-01 16:09:33 -0700

    Apply generic typing to extension function

--- src/main/kotlin/com/huangsam/person/Professional.kt
+++ src/main/kotlin/com/huangsam/person/Professional.kt
@@ -2,7 +2,7 @@ package com.huangsam.person
 
 // Additional context on internal visibility
 // https://kotlinlang.org/docs/visibility-modifiers.html#modules
-internal fun Professional.isIntern(): Boolean = !isTrusted()
+internal fun <T : Professional> T.isIntern(): Boolean = !isTrusted()
 
 abstract class Professional(
     name: String,
