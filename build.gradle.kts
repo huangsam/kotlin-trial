2603c6e581bd2e8049ab3cbadf909c43e33e3052
Samuel Huang <samhuang91@gmail.com>
2024-11-22 09:09:12 -0800

    Update gradle to use version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.0.21"
+    kotlin("jvm") version "2.0.21"
 
     // https://github.com/ben-manes/gradle-versions-plugin
-    id("com.github.ben-manes.versions") version "0.51.0"
+    alias(libs.plugins.gradle.versions)
 
     // https://docs.gradle.org/current/userguide/application_plugin.html
     application
@@ -21,29 +21,24 @@ repositories {
 
 dependencies {
     // https://www.slf4j.org/manual.html
-    implementation("org.slf4j:slf4j-api:2.0.16")
-    implementation("ch.qos.logback:logback-classic:1.5.12")
+    implementation(libs.sl4j.api)
+    implementation(libs.logback.classic)
 
     // https://github.com/Kotlin/kotlinx.coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    implementation(libs.bundles.kotlin.coro.all)
 
     // https://github.com/google/gson/tree/main
-    implementation("com.google.code.gson:gson:2.11.0")
+    implementation(libs.gson)
 
     // https://www.baeldung.com/mockito-junit-5-extension
-    testImplementation("org.mockito:mockito-core:5.14.2")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
-
     // https://github.com/mockito/mockito-kotlin
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation(libs.bundles.mockito.all)
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation(libs.junit.jupiter)
 
     // Referenced here until Gradle 9.x is official
     // https://docs.gradle.org/8.3/userguide/upgrading_version_8.html
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly(libs.junit.launcher)
 }
 
 kotlin {
@@ -60,10 +55,6 @@ tasks.named<Test>("test") {
 
 tasks.test {
     finalizedBy(tasks.jacocoTestReport)
-}
-
-jacoco {
-    toolVersion = "0.8.11"
 }
 
 tasks.jacocoTestReport {
