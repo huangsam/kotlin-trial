feb86b6d8de2b4d7eb912cebd500ab14a75eea91
Samuel Huang <samhuang91@gmail.com>
2025-06-06 12:18:31 -0700

    Update kotlin and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.1.21"
 
     // https://github.com/ben-manes/gradle-versions-plugin
     alias(libs.plugins.gradle.versions)
