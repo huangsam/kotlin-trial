7438b650b1245785d3c862f953ec84613518c8dc
Samuel Huang <samhuang91@gmail.com>
2025-10-22 06:34:30 -0700

    Update kotlin to 2.2.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.2.10"
+    kotlin("jvm") version "2.2.20"
 
     // https://github.com/ben-manes/gradle-versions-plugin
     alias(libs.plugins.gradle.versions)
